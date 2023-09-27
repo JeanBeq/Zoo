356ea60e02917e878b1c2bc9c44a744a5f4fab1b
khababaa <102924962+khababaa@users.noreply.github.com>
2023-09-27 13:48:27 +0200

    Back: requete à la base de donnée operationel

--- api-zoo/routes/index.js
+++ api-zoo/routes/index.js
@@ -1,9 +1,48 @@
+const { Console } = require('console');
 var express = require('express');
 var router = express.Router();
-
+const fs = require('fs');
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+const cheminFichier = 'bddzoo.json';
+fs.readFile(cheminFichier, 'utf8', (err, data) => {
 
-module.exports = router;
+    router.get('/', function(req, res, next) {
+    
+        if (err) {
+          res.send('Erreur lors de la lecture du fichier :');
+          return;
+        }
+      
+        // Analyser les données JSON
+        try {
+          const jsonData = JSON.parse(data);
+          res.json(data);
+        } catch (parseError) {
+          console.error('Erreur lors de lanalyse JSON :', parseError);
+        }
+    
+    });
+    
+    router.get('/:catanimal', async (req, res) => {
+        const catanimal = req.params.catanimal;
+        
+
+
+        if (err) {
+            res.send('Erreur lors de la lecture du fichier :', err);
+            return;
+          }
+        
+          // Analyser les données JSON
+          try {
+            const animalcatData = JSON.parse(data);
+            
+            res.json(animalcatData[catanimal]);
+          } catch (parseError) {
+            res.send('Erreur lors de lanalyse JSON :', parseError);
+          }
+      });
+
+    });
+
+    module.exports = router;
