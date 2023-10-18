53c0689adb5f52506e1b8d8d12bce03a848fc2bd
Jean B <jean.bequilleux@gmail.com>
2023-10-18 09:12:58 +0200

    ajout cors api

--- api-zoo/app.js
+++ api-zoo/app.js
@@ -2,6 +2,8 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const cors = require('cors');
+
 
 var indexRouter = require('./routes/index');
 
@@ -9,6 +11,7 @@ var indexRouter = require('./routes/index');
 var app = express();
 
 app.use(logger('dev'));
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
